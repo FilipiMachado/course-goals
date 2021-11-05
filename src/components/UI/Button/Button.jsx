185eb1662727e8951495f08159635a6a3e998efc
Filipi <filipisee@hotmail.com>
2021-11-05 16:37:37 -0300

    added styled components

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+//import React from "react";
 // Styles
-import "./Button.css";
+//import "./Button.css";
+import styled from "styled-components";
 
-const Button = ({ type, onClick, children }) => {
+const Button = styled.button``;
+
+/*const Button = ({ type, onClick, children }) => {
   return (
     <button type={type} className="button" onClick={onClick}>
       {children}
     </button>
   );
 };
-
+*/
 export default Button;
