a93da8a5eff149ff9b89f4ded2962c891bd4a4cf
fiil <filipisee@hotmail.com>
2021-11-08 15:04:09 -0300

    added button styles with styled components

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -3,7 +3,28 @@
 //import "./Button.css";
 import styled from "styled-components";
 
-const Button = styled.button``;
+const Button = styled.button`
+  .button {
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #8b005d;
+    color: white;
+    background: #8b005d;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
+  }
+
+  .button:focus {
+    outline: none;
+  }
+
+  .button:hover,
+  .button:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
 
 /*const Button = ({ type, onClick, children }) => {
   return (
